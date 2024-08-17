0f963b8a08f6ac53703df15511c6f18d62deacdc
bunnyguy <pipandw@gmail.com>
2024-08-17 10:16:24 -0500

    pingmenu changes

--- scripts/pingmenu.gd
+++ scripts/pingmenu.gd
@@ -15,8 +15,23 @@ func sethovervalue(spriteimg:String):
 	else: hover = 8
 	
 func _process(_delta):
-	print(animated_sprite_2d)
 	if Input.is_action_just_pressed("openping"):
 		animated_sprite_2d.play("open")
 	if Input.is_action_just_released("openping"):
+		print(hover)
+		hover = 0
 		animated_sprite_2d.play("close")
+
+
+func _on_pingbutton_1_mouse_entered(): hover = 1;
+func _on_pingbutton_2_mouse_entered(): hover = 2
+func _on_pingbutton_3_mouse_entered(): hover = 3
+func _on_pingbutton_4_mouse_entered(): hover = 4
+func _on_pingbutton_5_mouse_entered(): hover = 5
+func _on_pingbutton_6_mouse_entered(): hover = 6
+func _on_pingbutton_7_mouse_entered(): hover = 7
+func _on_pingbutton_8_mouse_entered(): hover = 8
+
+
+func _on_pingbutton_1_mouse_exited(caller):
+	print(caller)
