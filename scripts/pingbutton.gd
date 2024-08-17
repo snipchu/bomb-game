ee0053d86dab833eb714c5adcfbb9622d45f366d
bunnyguy <pipandw@gmail.com>
2024-08-16 21:36:19 -0500

    pingmenu animation (but its broken)

--- scripts/pingbutton.gd
+++ scripts/pingbutton.gd
@@ -1,9 +1,10 @@
 extends Area2D
 
+@onready var sprite_2d = $Sprite2D
 
 func _on_mouse_entered():
 	scale = Vector2(scale.x+.25,scale.y+.25)
-	Pingmenu.hover = 1
-
+	Pingmenu.sethovervalue(sprite_2d.texture.get_path().get_file())
+	
 func _on_mouse_exited():
 	scale = Vector2(scale.x-.25,scale.y-.25)
--- scripts/pingmenu.gd
+++ scripts/pingmenu.gd
@@ -1,10 +1,22 @@
 extends Container
 
-var hover = 0
-func _process(delta):
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+var hover = 0;
+
+func sethovervalue(spriteimg:String):
+	if spriteimg=="red.png": hover = 1
+	elif spriteimg=="orange.png": hover = 2
+	elif spriteimg=="yellow.png": hover = 3
+	elif spriteimg=="green.png": hover = 4
+	elif spriteimg=="cyan.png": hover = 5
+	elif spriteimg=="blue.png": hover = 6
+	elif spriteimg=="purple.png": hover = 7
+	else: hover = 8
+	
+func _process(_delta):
+	print(animated_sprite_2d)
 	if Input.is_action_just_pressed("openping"):
-		visible = true
+		animated_sprite_2d.play("open")
 	if Input.is_action_just_released("openping"):
-		print(hover)
-		hover = 0
-		visible = false
+		animated_sprite_2d.play("close")
